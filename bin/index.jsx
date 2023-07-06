02f3184b24a43c82fc9933d9611b04dce1037a28
t.inoue <takayuki.inoue@i-studio.co.jp>
2023-07-06 15:39:24 +0900

    fix

--- bin/index.jsx
+++ bin/index.jsx
@@ -1,8 +1,7 @@
-import { jsx as _jsx } from "@builder.io/qwik/jsx-runtime";
 import { extractCritical } from "@emotion/server";
 import { renderToString } from "@builder.io/qwik/server";
 const createEmotionRoot = async (Root, options) => {
-    const render = await renderToString(_jsx(Root, {}), options);
+    const render = await renderToString(<Root />, options);
     const { css, ids } = extractCritical(render.html);
     return { css, ids };
 };
